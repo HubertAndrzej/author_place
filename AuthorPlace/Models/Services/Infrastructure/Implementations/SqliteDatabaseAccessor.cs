986d01760f2d1270052fff47110cc95e80755eae
Hubert Andrzej Sobolewski <hubertandrzejsobolewski@gmail.com>
2024-01-29 23:41:33 +0100

    Added Infrastucture Service hierarchy

---
+++ AuthorPlace/Models/Services/Infrastructure/Implementations/SqliteDatabaseAccessor.cs
@@ -0,0 +1,12 @@
+﻿using AuthorPlace.Models.Services.Infrastructure.Interfaces;
+using System.Data;
+
+namespace AuthorPlace.Models.Services.Infrastructure.Implementations;
+
+public class SqliteDatabaseAccessor : IDatabaseAccessor
+{
+    public DataSet Query(string query)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AuthorPlace/Models/Services/Infrastructure/Interfaces/IDatabaseAccessor.cs
@@ -0,0 +1,8 @@
+﻿using System.Data;
+
+namespace AuthorPlace.Models.Services.Infrastructure.Interfaces;
+
+public interface IDatabaseAccessor
+{
+    public DataSet Query(string query);
+}
