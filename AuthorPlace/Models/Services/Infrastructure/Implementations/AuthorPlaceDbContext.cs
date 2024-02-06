efd8acb7f21be03cc13c7d2de46d4fa06b1c7a21
Hubert Andrzej Sobolewski <hubertandrzejsobolewski@gmail.com>
2024-02-06 23:45:40 +0100

    Removed unnecessary code from AuthorPlaceDbContext

--- AuthorPlace/Models/Services/Infrastructure/Implementations/AuthorPlaceDbContext.cs
+++ AuthorPlace/Models/Services/Infrastructure/Implementations/AuthorPlaceDbContext.cs
@@ -23,41 +23,9 @@ public partial class AuthorPlaceDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Album>(entity =>
-        {
-            entity.Property(e => e.Author).HasColumnType("TEXT (100)");
-            entity.Property(e => e.CurrentPriceAmount)
-                .HasDefaultValueSql("0")
-                .HasColumnType("NUMERIC")
-                .HasColumnName("CurrentPrice_Amount");
-            entity.Property(e => e.CurrentPriceCurrency)
-                .HasDefaultValueSql("'EUR'")
-                .HasColumnType("TEXT (3)")
-                .HasColumnName("CurrentPrice_Currency");
-            entity.Property(e => e.Description).HasColumnType("TEXT (10000)");
-            entity.Property(e => e.Email).HasColumnType("TEXT (100)");
-            entity.Property(e => e.FullPriceAmount)
-                .HasDefaultValueSql("0")
-                .HasColumnType("NUMERIC")
-                .HasColumnName("FullPrice_Amount");
-            entity.Property(e => e.FullPriceCurrency)
-                .HasDefaultValueSql("'EUR'")
-                .HasColumnType("TEXT (3)")
-                .HasColumnName("FullPrice_Currency");
-            entity.Property(e => e.ImagePath).HasColumnType("TEXT (100)");
-            entity.Property(e => e.Title).HasColumnType("TEXT (100)");
-        });
+        modelBuilder.Entity<Album>();
 
-        modelBuilder.Entity<Song>(entity =>
-        {
-            entity.Property(e => e.Description).HasColumnType("TEXT (10000)");
-            entity.Property(e => e.Duration)
-                .HasDefaultValueSql("'00:00:00'")
-                .HasColumnType("TEXT (8)");
-            entity.Property(e => e.Title).HasColumnType("TEXT (100)");
-
-            entity.HasOne(d => d.Album).WithMany(p => p.Songs).HasForeignKey(d => d.AlbumId);
-        });
+        modelBuilder.Entity<Song>();
 
         OnModelCreatingPartial(modelBuilder);
     }
