b4ce71e6a5f98fa38edde4cbd6cfb1a9de24abdf
Hubert Andrzej Sobolewski <hubertandrzejsobolewski@gmail.com>
2024-02-07 00:03:36 +0100

    Added mapping configuration in AuthorPlaceDbContext

--- AuthorPlace/Models/Services/Infrastructure/Implementations/AuthorPlaceDbContext.cs
+++ AuthorPlace/Models/Services/Infrastructure/Implementations/AuthorPlaceDbContext.cs
@@ -23,9 +23,29 @@ public partial class AuthorPlaceDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Album>();
+        modelBuilder.Entity<Album>(entity =>
+        {
+            entity.ToTable("Albums");
+            entity.HasKey(album => album.Id);
+            entity.OwnsOne(album => album.CurrentPrice, builder =>
+            {
+                builder.Property(money => money.Amount).HasColumnName("CurrentPrice_Amount");
+                builder.Property(money => money.Currency).HasConversion<string>().HasColumnName("CurrentPrice_Currency");
+            });
+            entity.OwnsOne(album => album.FullPrice, builder =>
+            {
+                builder.Property(money => money.Amount).HasColumnName("FullPrice_Amount");
+                builder.Property(money => money.Currency).HasConversion<string>().HasColumnName("FullPrice_Currency");
+            });
+            entity.HasMany(album => album.Songs).WithOne(song => song.Album).HasForeignKey(song => song.AlbumId);
+        });
 
-        modelBuilder.Entity<Song>();
+        modelBuilder.Entity<Song>(entity =>
+        {
+            entity.ToTable("Songs");
+            entity.HasKey(song => song.Id);
+            entity.HasOne(song => song.Album).WithMany(album => album.Songs).HasForeignKey(song => song.AlbumId);
+        });
 
         OnModelCreatingPartial(modelBuilder);
     }
