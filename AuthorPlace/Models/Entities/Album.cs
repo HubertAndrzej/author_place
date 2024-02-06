50451ce7d1ee267aaf658f94cc782a39ed9c3989
Hubert Andrzej Sobolewski <hubertandrzejsobolewski@gmail.com>
2024-02-06 22:34:53 +0100

    Autogenerated Album and Song entities and AuthorPlaceDbContext

---
+++ AuthorPlace/Models/Entities/Album.cs
@@ -0,0 +1,17 @@
+﻿namespace AuthorPlace.Models.Entities;
+
+public class Album
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string? Description { get; set; }
+    public string? ImagePath { get; set; }
+    public string Author { get; set; } = null!;
+    public string? Email { get; set; }
+    public double Rating { get; set; }
+    public string FullPriceAmount { get; set; } = null!;
+    public string FullPriceCurrency { get; set; } = null!;
+    public string CurrentPriceAmount { get; set; } = null!;
+    public string CurrentPriceCurrency { get; set; } = null!;
+    public virtual ICollection<Song> Songs { get; set; } = new List<Song>();
+}
---
+++ AuthorPlace/Models/Services/Infrastructure/Implementations/AuthorPlaceDbContext.cs
@@ -0,0 +1,66 @@
+﻿using AuthorPlace.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthorPlace.Models.Services.Infrastructure.Implementations;
+
+public partial class AuthorPlaceDbContext : DbContext
+{
+    public AuthorPlaceDbContext()
+    {
+
+    }
+
+    public AuthorPlaceDbContext(DbContextOptions<AuthorPlaceDbContext> options) : base(options)
+    {
+
+    }
+
+    public virtual DbSet<Album>? Albums { get; set; }
+
+    public virtual DbSet<Song>? Songs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite("Data Source = Data/AuthorPlace.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Album>(entity =>
+        {
+            entity.Property(e => e.Author).HasColumnType("TEXT (100)");
+            entity.Property(e => e.CurrentPriceAmount)
+                .HasDefaultValueSql("0")
+                .HasColumnType("NUMERIC")
+                .HasColumnName("CurrentPrice_Amount");
+            entity.Property(e => e.CurrentPriceCurrency)
+                .HasDefaultValueSql("'EUR'")
+                .HasColumnType("TEXT (3)")
+                .HasColumnName("CurrentPrice_Currency");
+            entity.Property(e => e.Description).HasColumnType("TEXT (10000)");
+            entity.Property(e => e.Email).HasColumnType("TEXT (100)");
+            entity.Property(e => e.FullPriceAmount)
+                .HasDefaultValueSql("0")
+                .HasColumnType("NUMERIC")
+                .HasColumnName("FullPrice_Amount");
+            entity.Property(e => e.FullPriceCurrency)
+                .HasDefaultValueSql("'EUR'")
+                .HasColumnType("TEXT (3)")
+                .HasColumnName("FullPrice_Currency");
+            entity.Property(e => e.ImagePath).HasColumnType("TEXT (100)");
+            entity.Property(e => e.Title).HasColumnType("TEXT (100)");
+        });
+
+        modelBuilder.Entity<Song>(entity =>
+        {
+            entity.Property(e => e.Description).HasColumnType("TEXT (10000)");
+            entity.Property(e => e.Duration)
+                .HasDefaultValueSql("'00:00:00'")
+                .HasColumnType("TEXT (8)");
+            entity.Property(e => e.Title).HasColumnType("TEXT (100)");
+
+            entity.HasOne(d => d.Album).WithMany(p => p.Songs).HasForeignKey(d => d.AlbumId);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
