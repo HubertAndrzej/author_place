9e05861dc3ed9047ed57e191d2a19b8472623532
Hubert Andrzej Sobolewski <hubertandrzejsobolewski@gmail.com>
2024-02-09 14:52:14 +0100

    Added Configuration

---
+++ AuthorPlace/Models/Options/AlbumsOrderOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthorPlace.Models.Options;
+
+public class AlbumsOrderOptions
+{
+    public string? By { get; set; }
+    public bool Ascending { get; set; }
+    public string[]? Allow { get; set; }
+}
