4e2baee4ee6f77d549e5c49ecb3dd76f9312388a
Hubert Andrzej Sobolewski <hubertandrzejsobolewski@gmail.com>
2024-02-10 23:32:03 +0100

    Added distibuted cache in memory configuration

---
+++ AuthorPlace/Models/Services/Application/Implementations/DistributedCacheAlbumService.cs
@@ -0,0 +1,56 @@
+﻿using AuthorPlace.Models.Services.Application.Interfaces;
+using AuthorPlace.Models.ViewModels;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace AuthorPlace.Models.Services.Application.Implementations;
+
+public class DistributedCacheAlbumService : ICachedAlbumService
+{
+    private readonly IAlbumService albumService;
+    private readonly IDistributedCache distributedCache;
+
+    public DistributedCacheAlbumService(IAlbumService albumService, IDistributedCache distributedCache)
+    {
+        this.albumService = albumService;
+        this.distributedCache = distributedCache;
+    }
+
+    public async Task<List<AlbumViewModel>> GetAlbumsAsync()
+    {
+        string key = $"Album";
+        string serializedObject = await distributedCache.GetStringAsync(key);
+        if (serializedObject == null)
+        {
+            List<AlbumViewModel> albums = await albumService.GetAlbumsAsync();
+            serializedObject = JsonConvert.SerializeObject(albums);
+            await distributedCache.SetStringAsync(key, serializedObject);
+            return albums;
+        }
+        else
+        {
+            return JsonConvert.DeserializeObject<List<AlbumViewModel>>(serializedObject)!;
+        }
+    }
+
+    public async Task<AlbumDetailViewModel> GetAlbumAsync(int id)
+    {
+        string key = $"Album{id}";
+        string serializedObject = await distributedCache.GetStringAsync(key);
+        if (serializedObject == null)
+        {
+            AlbumDetailViewModel album = await albumService.GetAlbumAsync(id);
+
+            serializedObject = JsonConvert.SerializeObject(album);
+            DistributedCacheEntryOptions cacheOptions = new();
+            cacheOptions.SetAbsoluteExpiration(TimeSpan.FromSeconds(60));
+            await distributedCache.SetStringAsync(key, serializedObject, cacheOptions);
+            return album;
+        }
+        else
+        {
+            return JsonConvert.DeserializeObject<AlbumDetailViewModel>(serializedObject)!;
+        }
+    }
+}
--- AuthorPlace/Program.cs
+++ AuthorPlace/Program.cs
@@ -12,15 +12,16 @@ builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnCh
 builder.Host.UseSerilog((hostContext, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostContext.Configuration));
 builder.Services.AddMvc(options =>
 {
-    CacheProfile cacheProfile = new CacheProfile();
+    CacheProfile cacheProfile = new();
     builder.Configuration.Bind("ResponseCache:Home", cacheProfile);
     options.CacheProfiles.Add("Home", cacheProfile);
 });
 builder.Services.AddScoped<IAlbumService, EFCoreAlbumService>();
 builder.Services.AddDbContextPool<AuthorPlaceDbContext>(optionsBuilder => optionsBuilder.UseSqlite(builder.Configuration.GetConnectionString("Default")!));
 builder.Services.AddSingleton<IErrorViewSelectorService, ErrorViewSelectorService>();
-builder.Services.AddTransient<ICachedAlbumService, MemoryCacheAlbumService>();
+builder.Services.AddTransient<ICachedAlbumService, DistributedCacheAlbumService>();
 builder.Services.AddResponseCaching();
+builder.Services.AddDistributedMemoryCache();
 builder.Services.Configure<ConnectionStringsOptions>(builder.Configuration.GetSection("ConnectionStrings"));
 builder.Services.Configure<AlbumsOptions>(builder.Configuration.GetSection("Albums"));
 builder.Services.Configure<MemoryCacheOptions>(builder.Configuration.GetSection("MemoryCache"));
