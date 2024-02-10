75231602f27d739a1151261aaf1c2cce87e47c08
Hubert Andrzej Sobolewski <hubertandrzejsobolewski@gmail.com>
2024-02-10 21:35:08 +0100

    Added caching configuration

---
+++ AuthorPlace/Models/Services/Application/Implementations/MemoryCachedAlbumService.cs
@@ -0,0 +1,37 @@
+﻿using AuthorPlace.Models.Services.Application.Interfaces;
+using AuthorPlace.Models.ViewModels;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace AuthorPlace.Models.Services.Application.Implementations;
+
+public class MemoryCachedAlbumService : ICachedAlbumService
+{
+    private readonly IAlbumService albumService;
+    private readonly IMemoryCache memoryCache;
+
+    public MemoryCachedAlbumService(IAlbumService albumService, IMemoryCache memoryCache)
+    {
+        this.albumService = albumService;
+        this.memoryCache = memoryCache;
+    }
+
+    public Task<List<AlbumViewModel>> GetAlbumsAsync()
+    {
+        return memoryCache.GetOrCreateAsync($"Albums", cacheEntry =>
+        {
+            cacheEntry.SetSize(1);
+            cacheEntry.SetAbsoluteExpiration(TimeSpan.FromSeconds(60));
+            return albumService.GetAlbumsAsync();
+        });
+    }
+
+    public Task<AlbumDetailViewModel> GetAlbumAsync(int id)
+    {
+        return memoryCache.GetOrCreateAsync($"Album{id}", cacheEntry =>
+        {
+            cacheEntry.SetSize(1);
+            cacheEntry.SetAbsoluteExpiration(TimeSpan.FromSeconds(60));
+            return albumService.GetAlbumAsync(id);
+        });
+    }
+}
--- AuthorPlace/Program.cs
+++ AuthorPlace/Program.cs
@@ -2,22 +2,33 @@ using AuthorPlace.Models.Options;
 using AuthorPlace.Models.Services.Application.Implementations;
 using AuthorPlace.Models.Services.Application.Interfaces;
 using AuthorPlace.Models.Services.Infrastructure.Implementations;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using Serilog;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 builder.Host.UseSerilog((hostContext, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostContext.Configuration));
-builder.Services.AddMvc();
+builder.Services.AddMvc(options =>
+{
+    CacheProfile cacheProfile = new CacheProfile();
+    builder.Configuration.Bind("ResponseCache:Home", cacheProfile);
+    options.CacheProfiles.Add("Home", cacheProfile);
+});
 builder.Services.AddScoped<IAlbumService, EFCoreAlbumService>();
 builder.Services.AddDbContextPool<AuthorPlaceDbContext>(optionsBuilder => optionsBuilder.UseSqlite(builder.Configuration.GetConnectionString("Default")!));
 builder.Services.AddSingleton<IErrorViewSelectorService, ErrorViewSelectorService>();
+builder.Services.AddTransient<ICachedAlbumService, MemoryCachedAlbumService>();
+builder.Services.AddResponseCaching();
 builder.Services.Configure<ConnectionStringsOptions>(builder.Configuration.GetSection("ConnectionStrings"));
 builder.Services.Configure<AlbumsOptions>(builder.Configuration.GetSection("Albums"));
+builder.Services.Configure<MemoryCacheOptions>(builder.Configuration.GetSection("MemoryCache"));
 
 WebApplication app = builder.Build();
 app.UseExceptionHandler("/Error");
 app.UseStaticFiles();
+app.UseResponseCaching();
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 app.MapFallbackToController("{*path}", "Index", "Error");
 app.Run();
