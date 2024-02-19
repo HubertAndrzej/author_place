506a329d94e6ff98751a3761e490b07d3ddda37d
Hubert Andrzej Sobolewski <hubertandrzejsobolewski@gmail.com>
2024-02-19 18:52:21 +0100

    Created View Component

---
+++ AuthorPlace/Customizations/ViewComponents/PaginationBarViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using AuthorPlace.Models.ViewModels;
+using AuthorPlace.Models.ViewModels.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthorPlace.Customizations.ViewComponents;
+
+public class PaginationBarViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(IPaginationInfo model)
+    {
+        return View(model);
+    }
+}
