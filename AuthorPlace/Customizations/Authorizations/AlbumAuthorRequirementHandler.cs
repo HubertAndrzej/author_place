cdb330a02c7e5c99118ad4aca79b4815071ea017
Hubert Andrzej Sobolewski <hubertandrzejsobolewski@gmail.com>
2024-03-11 17:05:21 +0100

    Enhanced requirement handler to manage songs

--- AuthorPlace/Customizations/Authorizations/AlbumAuthorRequirementHandler.cs
+++ AuthorPlace/Customizations/Authorizations/AlbumAuthorRequirementHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AuthorPlace.Models.Services.Application.Interfaces.Albums;
+using AuthorPlace.Models.Services.Application.Interfaces.Songs;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 
@@ -8,21 +9,43 @@ namespace AuthorPlace.Customizations.Authorizations
     {
         private readonly IHttpContextAccessor httpContextAccessor;
         private readonly ICachedAlbumService albumService;
+        private readonly ISongService songService;
 
-        public AlbumAuthorRequirementHandler(IHttpContextAccessor httpContextAccessor, ICachedAlbumService albumService)
+        public AlbumAuthorRequirementHandler(IHttpContextAccessor httpContextAccessor, ICachedAlbumService albumService, ISongService songService)
         {
             this.httpContextAccessor = httpContextAccessor;
             this.albumService = albumService;
+            this.songService = songService;
         }
 
         protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, AlbumAuthorRequirement requirement)
         {
             string userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
-            int albumId = context.Resource is int i ? i : Convert.ToInt32(httpContextAccessor.HttpContext!.Request.RouteValues["id"]);
-            if (albumId == 0)
+            int albumId;
+            if (context.Resource is int i)
             {
-                context.Fail();
-                return;
+                albumId = i;
+            }
+            else
+            {
+                int id = Convert.ToInt32(httpContextAccessor.HttpContext!.Request.RouteValues["id"]);
+                if (id == 0)
+                {
+                    context.Fail();
+                    return;
+                }
+                switch (httpContextAccessor.HttpContext.Request.RouteValues["controller"]!.ToString()!.ToLowerInvariant())
+                {
+                    case "songs":
+                        albumId = (await songService.GetSongAsync(id)).AlbumId;
+                        break;
+                    case "courses":
+                        albumId = id;
+                        break;
+                    default:
+                        context.Fail();
+                        return;
+                }
             }
             string authorId = await albumService.GetAlbumAuthorIdAsync(albumId);
             if (userId == authorId)
